c1e987d6997c101401215078a1fd3bfb6de06b8e
itsLeo <107639845+itsJhnL@users.noreply.github.com>
2023-10-10 19:41:52 +0800

    link changed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -59,7 +59,7 @@ export default function HomePage() {
                 {clicked ? "Scrolling..." : "Let's work together!"}
               </NavLink>
               <a
-                href="https://drive.google.com/file/d/1DW9Gf6WIY69dQx6Awp_ugUIIvmidZYi8/view?usp=sharing"
+                href="https://drive.google.com/file/d/1k5sZ5ZYl4GGZ6OlfRsfEXX9gI5_AI3Nx/view?usp=sharing"
                 target="blank"
                 rel="noopenner"
                 className="rounded-lg text-base font-medium border border-[#a3262e] text-[#a3262e] p-2 px-6 hover:bg-rose-50"

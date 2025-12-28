a97478b0088cd01c255518d1ea90f282f56ad0e8
itsJhnl <brunojohn0004@gmail.com>
2025-12-29 00:29:32 +0800

    added maintenance page

--- src/maintenance.jsx
+++ src/maintenance.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function maintenance() {
     return (
         <div className="flex justify-center items-center font-bold mx-auto h-screen w-screen">
-            <p>Website Under Construction!</p>
+            <p>Website Under Maintenance!</p>
         </div>
     )
 }
